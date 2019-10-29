66aaee6daab12f71dc679013b5c631fc08c84708
knoxgon <volkan.guven@hotmail.se>
2019-10-29 09:46:10 +0100

    Added libraries

--- script.js
+++ script.js
@@ -3,19 +3,17 @@ let sck = io.connect('http://localhost:1234');
 let WindowGraphic = new Graphic(document.getElementById("myCanvas"), document.getElementById("myCanvas").getContext("2d"));
 let keys = []
 let listOfPlayers = {};
-
+const MOUSE = {x:0,y:0}
 
 let randomNick = "PlayerNick_" + Math.random() % 999999999;
-let randomX = 35;
-let randomY = 35;
+let randomX = (Math.random() * 400) + 1;
+let randomY = (Math.random() * 400) + 1;
 
-let player = new Player(randomNick, randomX, randomY, 10, 1);
+let player = new Player(randomNick, randomX, randomY, 32, 32, 15, 1);
 sck.emit('new player spawn', player);
 
-let tmap = new TileMap('gamelayer.json');
+let tmap = new TileMap('gamelayer.json', '../assets/terrain_atlas.png');
 tmap.readFile();
-
-//const coin = new SpriteAnimation('assets/coin-sprite-animation.png', 10, 4, 0.5);
 
 sck.on('player join', (data) => {
   //Iterate players
@@ -23,7 +21,7 @@ sck.on('player join', (data) => {
     //We dont want to recreate same player
     if(!listOfPlayers.hasOwnProperty(data[key].name)) {
       //Create new player asset
-      listOfPlayers[data[key].name] = new Player(data[key].name, data[key].x, data[key].y, data[key].radius, data[key].speed);
+      listOfPlayers[data[key].name] = new Player(data[key].name, data[key].x, data[key].y, data[key].width, data[key].height, data[key].radius, data[key].speed);
     }
   }
 });
@@ -41,25 +39,28 @@ document.addEventListener('keyup', (event) => {
   keys[event.keyCode] = false;
 });
 
+WindowGraphic.canvas.addEventListener('mousemove', (evt) => {
+  let rect = WindowGraphic.canvas.getBoundingClientRect();
+  MOUSE.x = evt.clientX - rect.left;
+  MOUSE.y = evt.clientY - rect.top;
+}, false);
+
+
 sck.on('player leave', (data) => {
   delete listOfPlayers[data.name];
-});
-
-sck.on('player shoot', (data) => {
-
 });
 
 function loop() {
   WindowGraphic.clear();
   tmap.render();
-  // player.move();
+  // player refresh
   for (let key in listOfPlayers) {
     if (listOfPlayers.hasOwnProperty(key)) {
       listOfPlayers[key].render();
     }
   };
   if(listOfPlayers[player.name])
-    listOfPlayers[player.name].move()
+    listOfPlayers[player.name].move();
   requestAnimationFrame(loop);  
 }
 requestAnimationFrame(loop);
